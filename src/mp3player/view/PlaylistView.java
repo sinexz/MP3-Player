f7399436fa622d66b068c67258b72c78288a5cd3
HqNeko <dennie.walo@gmail.com>
2017-12-14 00:00:20 +0100

    -Bisschen View

--- src/mp3player/view/PlaylistView.java
+++ src/mp3player/view/PlaylistView.java
@@ -1,11 +1,9 @@
 package mp3player.view;
 
 import javafx.geometry.Insets;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.SplitPane;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
@@ -55,36 +53,36 @@ public class PlaylistView extends SplitPane {
         setMinHeight(USE_PREF_SIZE);
         setMinWidth(USE_PREF_SIZE);
         setOrientation(javafx.geometry.Orientation.VERTICAL);
-        setPrefHeight(425.0);
-        setPrefWidth(480.0);
+        setPrefHeight(500.0);
+        setPrefWidth(500.0);
 
         anchorPane.setMaxHeight(USE_PREF_SIZE);
         anchorPane.setMaxWidth(USE_PREF_SIZE);
         anchorPane.setMinHeight(USE_PREF_SIZE);
         anchorPane.setMinWidth(USE_PREF_SIZE);
-        anchorPane.setPrefHeight(275.0);
-        anchorPane.setPrefWidth(480.0);
+        anchorPane.setPrefHeight(500.0);
+        anchorPane.setPrefWidth(500.0);
 
         splitPane.setDividerPositions(0.33);
         splitPane.setMaxHeight(USE_PREF_SIZE);
         splitPane.setMaxWidth(USE_PREF_SIZE);
         splitPane.setMinHeight(USE_PREF_SIZE);
         splitPane.setMinWidth(USE_PREF_SIZE);
-        splitPane.setPrefHeight(275.0);
-        splitPane.setPrefWidth(480.0);
+        splitPane.setPrefHeight(500.0);
+        splitPane.setPrefWidth(500.0);
 
         VBoxPlaylist.setMaxHeight(USE_PREF_SIZE);
         VBoxPlaylist.setMaxWidth(USE_PREF_SIZE);
         VBoxPlaylist.setMinHeight(USE_PREF_SIZE);
         VBoxPlaylist.setMinWidth(USE_PREF_SIZE);
-        VBoxPlaylist.setPrefHeight(275.0);
+        VBoxPlaylist.setPrefHeight(500.0);
         VBoxPlaylist.setPrefWidth(150.0);
 
         tableView.setMaxHeight(USE_PREF_SIZE);
         tableView.setMaxWidth(USE_PREF_SIZE);
         tableView.setMinHeight(USE_PREF_SIZE);
         tableView.setMinWidth(USE_PREF_SIZE);
-        tableView.setPrefHeight(225.0);
+        tableView.setPrefHeight(465.0);
         tableView.setPrefWidth(150.0);
 
         tableColumnPlaylists.setPrefWidth(150.0);
@@ -93,16 +91,17 @@ public class PlaylistView extends SplitPane {
         HBoxPlaylist.setAlignment(javafx.geometry.Pos.CENTER_LEFT);
         HBoxPlaylist.setPrefHeight(100.0);
         HBoxPlaylist.setPrefWidth(200.0);
+        HBoxPlaylist.setSpacing(10);
 
         labelAddPlaylist.setAlignment(javafx.geometry.Pos.TOP_LEFT);
         labelAddPlaylist.setMaxHeight(USE_PREF_SIZE);
         labelAddPlaylist.setMaxWidth(USE_PREF_SIZE);
         labelAddPlaylist.setMinHeight(USE_PREF_SIZE);
         labelAddPlaylist.setMinWidth(USE_PREF_SIZE);
-        labelAddPlaylist.setPrefWidth(80.0);
-        labelAddPlaylist.setText("Hinzuf�gen");
+        labelAddPlaylist.setPrefWidth(100.0);
+        labelAddPlaylist.setText("Hinzufügen");
         labelAddPlaylist.setFont(new Font(13.0));
-        labelAddPlaylist.setPadding(new Insets(0.0, 0.0, 0.0, 5.0));
+        labelAddPlaylist.setPadding(new Insets(0.0, 0.0, 0.0, 15.0));
 
         buttonAddPlaylist.setAlignment(javafx.geometry.Pos.CENTER_RIGHT);
         buttonAddPlaylist.setContentDisplay(javafx.scene.control.ContentDisplay.RIGHT);
@@ -113,30 +112,26 @@ public class PlaylistView extends SplitPane {
         buttonAddPlaylist.setMnemonicParsing(false);
         buttonAddPlaylist.setPrefHeight(25.0);
         buttonAddPlaylist.setPrefWidth(40.0);
-        buttonAddPlaylist.setText("+");
-        buttonAddPlaylist.setFont(new Font(14.0));
+        buttonAddPlaylist.setGraphic(new ImageView(new Image("file:src/mp3player/view/img/playlistAdd.png",
+                buttonAddPlaylist.getPrefWidth(),buttonAddPlaylist.getPrefHeight(),true,true)));
+        buttonAddPlaylist.setContentDisplay(ContentDisplay.CENTER);
+        buttonAddPlaylist.setStyle("-fx-background-color: transparent;");
 
-        imageView.setFitHeight(25.0);
-        imageView.setFitWidth(45.0);
-        imageView.setNodeOrientation(javafx.geometry.NodeOrientation.INHERIT);
-        imageView.setPickOnBounds(true);
-        imageView.setPreserveRatio(true);
-        buttonAddPlaylist.setGraphic(imageView);
-        buttonAddPlaylist.setPadding(new Insets(0.0, 0.0, 0.0, 10.0));
+
 
         hBox0.setAlignment(javafx.geometry.Pos.CENTER);
         hBox0.setMaxHeight(USE_PREF_SIZE);
         hBox0.setMaxWidth(USE_PREF_SIZE);
         hBox0.setMinHeight(USE_PREF_SIZE);
         hBox0.setMinWidth(USE_PREF_SIZE);
-        hBox0.setPrefHeight(275.0);
+        hBox0.setPrefHeight(500.0);
         hBox0.setPrefWidth(350.0);
 
         tableViewPlaylist.setMaxHeight(USE_PREF_SIZE);
         tableViewPlaylist.setMaxWidth(USE_PREF_SIZE);
         tableViewPlaylist.setMinHeight(USE_PREF_SIZE);
         tableViewPlaylist.setMinWidth(USE_PREF_SIZE);
-        tableViewPlaylist.setPrefHeight(275.0);
+        tableViewPlaylist.setPrefHeight(500.0);
         tableViewPlaylist.setPrefWidth(350.0);
 
         title.setMinWidth(100.0);
